1d9db3bbeaeb0bdb079ba833aa8bcfe0f1570edc
jaonineX <156872637+facezazaa@users.noreply.github.com>
2024-03-25 17:08:51 +0700

    ฟ

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # Show Data Index Price
 st.header("Show Data Index Price")
-df = pd.read_csv("./data/dummy_data.csv")
+df = pd.read_csv("./data/shopping_trends.csv")
 st.write(df.head(10))
 
 # Show Chart
